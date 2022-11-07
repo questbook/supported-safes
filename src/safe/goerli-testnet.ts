3c6743ceda7bb2fb5349827cda77269cb69c0475
sourav-bz <s.kumar001997@gmail.com>
2022-11-07 19:36:49 +0530

    testing goerli obj

--- src/safe/goerli-testnet.ts
+++ src/safe/goerli-testnet.ts
@@ -1,14 +1,14 @@
 import { gnosis } from "../utils/gnosis";
 
 export class GoerliTestnet extends gnosis {
-    chainName: string;
-    chainLogo: string;
 
-    constructor(safeAddress: string) {
-        super(5, 'https://safe-transaction.goerli.gnosis.io/api/', safeAddress);
-        this.chainName = 'Goerli Testnet'
-        this.chainLogo = 'https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/ethereum/info/logo.png'
-    }
+    chainName = 'Goerli Testnet'
+    chainLogo = 'https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/ethereum/info/logo.png'
+    // constructor(safeAddress: string) {
+    //     super(5, 'https://safe-transaction.goerli.gnosis.io/api/', safeAddress);
+    //     this.chainName = 'Goerli Testnet'
+    //     this.chainLogo = 'https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/ethereum/info/logo.png'
+    // }
 
     getChainId(){
         return this.chainId
