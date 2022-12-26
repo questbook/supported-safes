7917be8458f6f49ddcd2d028260754ef163b8ce1
sourav-bz <s.kumar001997@gmail.com>
2022-12-26 22:53:50 +0530

    ton send transaction with hash

--- src/wallets/TON.ts
+++ src/wallets/TON.ts
@@ -9,13 +9,15 @@ export class TonWallet {
 	provider: any
 	tonReady: boolean = false
 	address: string = ''
+    tonWeb: TonWeb
 
 	constructor() {
 		this.provider = null
 		this.tonReady = false
+        this.tonWeb = new TonWeb()
 	}
 
-	checkTonReady(window: any) {
+	checkTonReady = async(window: any) => {
 		if(window.ton) {
 			this.provider = window.ton
 			this.tonReady = true
@@ -26,47 +28,73 @@ export class TonWallet {
 				const account = accounts[0]
 				this.address = account
 			})
+			await sleep(1000)
 		}
 	}
 
 	connect = async() => {
 		const accounts = await this.provider.send('ton_requestAccounts')
-		console.log('TON accountsChanged', accounts)
 		const account = accounts[0]
 		this.address = account
-
-        const tonWeb = new TonWeb()
-		const wallet = tonWeb.wallet.create({ address: this.address })
-		const seqno = await wallet.methods.seqno().call()
-		console.log('TON seqno', seqno)
-
-        await sleep(1000)
-        
-		const transactions = await tonWeb.getTransactions(account, 20, undefined, undefined, undefined)
-		console.log('TON transactions', transactions)
-
-		console.log(await this.provider.send('ton_requestWallets'))
 	}
 
-	sendMoney = async(toAddress: string, amount: number) => {
+	sendMoney = async(toAddress: string, amount: number, callback: any) => {
 		try {
 			await this.connect()
+			await sleep(1000)
 			if(this.tonReady) {
+                const wallet = this.tonWeb.wallet.create({ address: this.address })
+				const prevSeqno = await wallet.methods.seqno().call()
+				console.log('TON seqno', prevSeqno)
+				await sleep(1000)
+
 				const result = await this.provider.send(
 					'ton_sendTransaction',
 					[{
 						to: toAddress,
-						value: amount * 10 ^ 8,
-						data: 'dapp test',
+						value: TonWeb.utils.toNano(amount.toString()).toNumber(),
+						data: 'questbook TON payout',
 						dataType: 'text'
 					}]
 				)
-				console.log('result', result)
-				return result
+
+				if(result){
+					await sleep(1000)
+					let startTime = new Date().getTime();
+					let interval = setInterval(async() => {
+						console.log('TON interval called');
+						if(new Date().getTime() - startTime > 30000){
+							clearInterval(interval);
+							callback({error: 'Transaction timeout'})
+						}
+	
+						const seqno = await wallet.methods.seqno().call()
+						console.log('TON seqno', seqno)
+						if(seqno > prevSeqno) {
+							await sleep(1000)
+							const transactions = await this.tonWeb.getTransactions(this.address, 5, undefined, undefined, undefined)
+							console.log('TON transactions after transaction', transactions)
+							let transactionHash = ''
+							for(let i = 0; i < transactions.length; i++){
+								if(transactions[i]?.in_msg?.message === "questbook TON payout"){
+									transactionHash = transactions[i].transaction_id.hash
+									break
+								}
+							}
+							console.log({transactionHash})
+							clearInterval(interval);
+							callback(result)
+						}
+					}, 1000)
+				}else{
+					callback({error: 'Transaction failed'})
+				}
+			}else{
+				callback({error: 'TON not ready'})
 			}
 		} catch(error) {
-			// User denied or Error
 			console.log(error)
+			callback({error: error.message})
 		}
 	}
 }
