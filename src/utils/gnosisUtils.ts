7787e0bfc0e357a2c2e38d5be56fa3d6468050e1
Richik Chanda <richikchanda1999@gmail.com>
2023-02-15 20:22:19 +0530

    chore: pass grant address when initiating transactions

--- src/utils/gnosisUtils.ts
+++ src/utils/gnosisUtils.ts
@@ -3,20 +3,20 @@ import { erc20ABI } from "wagmi"
 import { getCeloTokenUSDRate } from "./tokenConversionUtils"
 
 
-export const encodeTransactionData = (recipientAddress: string, fundAmount: string, rewardAssetDecimals: number, workspaceId: number, applicationId: number) =>  {
-	console.log('params for encodeTransactionData' ,fundAmount, rewardAssetDecimals)
+export const encodeTransactionData = (recipientAddress: string, fundAmount: string, rewardAssetDecimals: number, workspaceId: number, grantAddress: string, applicationId: number) =>  {
+	console.log('params for encodeTransactionData' ,workspaceId, applicationId, fundAmount, rewardAssetDecimals)
     const ERC20Interface = new ethers.utils.Interface(erc20ABI)
    let txData = ERC20Interface.encodeFunctionData('transfer', [
         recipientAddress,
         ethers.utils.parseUnits(fundAmount, rewardAssetDecimals)
     ])
 
-	txData = txData + ethers.utils.hexZeroPad(ethers.utils.hexlify(workspaceId), 32).slice(2) + ethers.utils.hexZeroPad(ethers.utils.hexlify(applicationId), 32).slice(2) + ethers.utils.hexZeroPad(ethers.utils.hexlify('0x5175657374626f6f6b'), 32).slice(2)
+	txData = txData + ethers.utils.hexZeroPad(ethers.utils.hexlify(workspaceId), 32).slice(2) + ethers.utils.hexZeroPad(ethers.utils.hexlify(grantAddress), 32).slice(2) + ethers.utils.hexZeroPad(ethers.utils.hexlify(applicationId), 32).slice(2) + ethers.utils.hexZeroPad(ethers.utils.hexlify('0x5175657374626f6f6b'), 32).slice(2)
 
     return txData
 }
 
-export const createEVMMetaTransactions = async (workspaceId: string, workspaceSafeChainId: string , gnosisBatchData: any): Promise<any[]> => {
+export const createEVMMetaTransactions = async (workspaceId: string, grantAddress: string, workspaceSafeChainId: string , gnosisBatchData: any): Promise<any[]> => {
 		const celoTokensUSDRateMapping = await (await getCeloTokenUSDRate()).data;
 		const readyTxs = gnosisBatchData.map((data: any) => {
 			let tokenUSDRate: number = 1
@@ -41,7 +41,8 @@ export const createEVMMetaTransactions = async (workspaceId: string, workspaceSa
 			const rewardAssetAddress = data.selectedToken.info.tokenAddress
 			const usdToToken = (data.amount / tokenUSDRate).toFixed(rewardAssetDecimals)
 
-			const txData = encodeTransactionData(data.to, (usdToToken.toString()), rewardAssetDecimals, parseInt(workspaceId, 16), data.applicationId)
+			console.log(workspaceId, 'Received workspace ID in string')
+			const txData = encodeTransactionData(data.to, (usdToToken.toString()), rewardAssetDecimals, parseInt(workspaceId, 16), grantAddress, data.applicationId)
 			const tx = {
 				to: ethers.utils.getAddress(rewardAssetAddress),
 				data: txData,
