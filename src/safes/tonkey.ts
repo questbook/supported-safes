468443e236b6f636393d0556486d52cb7d348b95
hasankhadra <59062695+hasankhadra@users.noreply.github.com>
2023-07-07 18:48:04 +0300

    fix: TON coin icon path

--- src/safes/tonkey.ts
+++ src/safes/tonkey.ts
@@ -257,7 +257,7 @@ export class tonkey implements SafeInterface {
         const tonUsdRate = await getTokenUSDonDate(this.TONTokenId, currentTime)
 
         list.push({
-            tokenIcon: undefined,
+            tokenIcon: '/icons/toncoin.svg',
             tokenName: 'TON',
             tokenValueAmount: undefined,
             usdValueAmount: balance,
