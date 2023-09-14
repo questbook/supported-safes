c7cd89f01dd1936ed9f4a0f339ecf8dacf2c220a
alimansour0002 <alimansour0002@gmail.com>
2023-09-14 04:22:30 +0300

    fixing waiting for transaction in TON

--- src/wallets/TON.ts
+++ src/wallets/TON.ts
@@ -2,9 +2,9 @@ import TonWeb from 'tonweb'
 import { sleep } from '../utils/general'
 
 export class TonWallet {
-    name: string = 'TON Wallet'
-    logo: string = '/wallet_icons/ton.png'
-    token: string = 'TON'
+	name: string = 'TON Wallet'
+	logo: string = '/wallet_icons/ton.png'
+	token: string = 'TON'
 	provider: any
 	tonReady: boolean = false
 	address: string = ''
@@ -14,8 +14,8 @@ export class TonWallet {
 		this.tonReady = false
 	}
 
-	checkTonReady = async(window: any) => {
-		if(window.ton) {
+	checkTonReady = async (window: any) => {
+		if (window.ton) {
 			this.provider = window.ton
 			this.tonReady = true
 			console.log('TON ready')
@@ -29,22 +29,24 @@ export class TonWallet {
 		}
 	}
 
-	connect = async() => {
+	connect = async () => {
 		const accounts = await this.provider.send('ton_requestAccounts')
 		const account = accounts[0]
 		this.address = account
 	}
 
-	sendMoney = async(toAddress: string, amount: number, isTestnet: boolean = false, callback: any) => {
+	sendMoney = async (toAddress: string, amount: number, isTestnet: boolean = false, callback: any) => {
 		try {
 			await this.connect()
 			await sleep(1000)
-			if(this.tonReady) {
+			if (this.tonReady) {
 				const tonWeb = new TonWeb(isTestnet ? new TonWeb.HttpProvider('https://testnet.toncenter.com/api/v2/jsonRPC') : new TonWeb.HttpProvider('https://toncenter.com/api/v2/jsonRPC'))
-                const wallet = tonWeb.wallet.create({ address: this.address })
+				const wallet = tonWeb.wallet.create({ address: this.address })
 				console.log(wallet.methods.seqno)
-				const prevSeqno = await wallet.methods.seqno().call()
-				console.log('TON seqno', prevSeqno)
+				const lastTx = (await tonWeb.getTransactions(this.address, 1))[0]
+				const lastTxHash = lastTx.transaction_id.hash
+
+				console.log('TON lastTx', lastTx, lastTxHash)
 				await sleep(1000)
 
 				const result = await this.provider.send(
@@ -56,44 +58,56 @@ export class TonWallet {
 						dataType: 'text'
 					}]
 				)
-
-				if(result){
+				
+				console.log('TON result', result)
+				
+				if (result) {
 					await sleep(1000)
-					let startTime = new Date().getTime();
-					let interval = setInterval(async() => {
-						console.log('TON interval called');
-						// if(new Date().getTime() - startTime > 30000){
-						// 	clearInterval(interval);
-						// 	callback({error: 'Transaction timeout'})
-						// }
-	
-						const seqno = await wallet.methods.seqno().call()
-						console.log('TON seqno', seqno)
-						if(seqno > prevSeqno) {
-							await sleep(1000)
-							const transactions = await tonWeb.getTransactions(this.address, 5, undefined, undefined, undefined)
-							console.log('TON transactions after transaction', transactions)
-							let transactionHash = ''
-							for(let i = 0; i < transactions.length; i++){
-								if(transactions[i]?.in_msg?.message === "questbook TON payout"){
-									transactionHash = transactions[i].transaction_id.hash
-									break
-								}
-							}
-							console.log({transactionHash})
-							clearInterval(interval);
-							callback({transactionHash})
-						}
-					}, 2000)
-				}else{
-					callback({error: 'Transaction failed'})
+					// let interval = setInterval(async() => {
+					// 	console.log('TON interval called');
+					// 	// if(new Date().getTime() - startTime > 30000){
+					// 	// 	clearInterval(interval);
+					// 	// 	callback({error: 'Transaction timeout'})
+					// 	// }
+
+					// 	const seqno = await wallet.methods.seqno().call()
+					// 	console.log('TON seqno', seqno)
+					// 	if(seqno > prevSeqno) {
+					// 		await sleep(1000)
+					// 		const transactions = await tonWeb.getTransactions(this.address, 5, undefined, undefined, undefined)
+					// 		console.log('TON transactions after transaction', transactions)
+					// 		let transactionHash = ''
+					// 		for(let i = 0; i < transactions.length; i++){
+					// 			if(transactions[i]?.in_msg?.message === "questbook TON payout"){
+					// 				transactionHash = transactions[i].transaction_id.hash
+					// 				break
+					// 			}
+					// 		}
+					// 		console.log({transactionHash})
+					// 		clearInterval(interval);
+					// 		callback({transactionHash})
+					// 	}
+					// }, 2000)
+					let txHash = lastTxHash
+					let tx = lastTx
+
+					while (txHash == lastTxHash && tx?.in_msg?.message === "questbook TON payout") {
+						await sleep(1500) // some delay between API calls
+						tx = (await tonWeb.getTransactions(this.address, 1))[0]
+						txHash = tx.transaction_id.hash
+					}
+					console.log('TON txHash', txHash)
+					callback({ transactionHash: txHash })
+
+				} else {
+					callback({ error: 'Transaction failed' })
 				}
-			}else{
-				callback({error: 'TON not ready'})
+			} else {
+				callback({ error: 'TON not ready' })
 			}
-		} catch(error) {
+		} catch (error) {
 			console.log(error)
-			callback({error: error.message})
+			callback({ error: error.message })
 		}
 	}
 }
