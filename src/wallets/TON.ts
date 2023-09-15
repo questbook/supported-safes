5434c084e2aeb2bd76f55d5aa3899c5f678bb3ec
alimansour0002 <alimansour0002@gmail.com>
2023-09-15 14:02:56 +0300

    fixing bugs

--- src/wallets/TON.ts
+++ src/wallets/TON.ts
@@ -123,7 +123,7 @@ export class TonWallet {
 	getToken = async () =>{
 		let list: TokenDetailsInterface
 
-
+		
         const currentTime = (new Date()).toLocaleDateString().split('/').join('-')
 
         const tonUsdRate = await getTokenUSDonDate(this.TONTokenId, currentTime)
